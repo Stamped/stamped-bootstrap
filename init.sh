b0bf369a417d2d74a28844a6969eba0b8141d0b5
Kevin Palms <kevin@stamped.com>
2011-07-07 17:51:40 -0400

    Update

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
 
-echo "script initiated" > /stamped-bootstrap/status
+echo "script initiated as `whoami`"
 
 echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | tee /etc/apt/sources.list.d/opscode.list
+echo "1"
 
 wget -qO - http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
+
 apt-get update
 
 apt-get -y install chef
 
-echo "chef installed" >> /stamped-bootstrap/status
+echo "chef installed"
