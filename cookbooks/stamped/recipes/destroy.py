67689a9c14b27bc44670c3187decb0f04e95f17f
Travis Fischer <travis@stamped.com>
2011-07-19 17:42:22 -0400

    .

--- cookbooks/stamped/recipes/destroy.py
+++ cookbooks/stamped/recipes/destroy.py
@@ -20,11 +20,11 @@ def check_shell(cmd, show_cmd=True):
 
 if 'db' in env.config.node.roles:
     # TODO: temporary
-    check_shell("ps -e | grep mongod | grep -v grep | sed 's/^\([0-9]*\).*/\1/g' | xargs kill -9")
+    check_shell(r"ps -e | grep mongod | grep -v grep | sed 's/^\([0-9]*\).*/\1/g' | xargs kill -9")
 
 if 'web_server' in env.config.node.roles:
     # start wsgi application (flask server)
     if 'wsgi' in env.config.node:
         # TODO
-        check_shell("ps -e | grep python | grep 'serve\.py' | grep -v grep | sed 's/^\([0-9]*\).*/\1/g' | xargs kill -9")
+        check_shell(r"ps -e | grep python | grep 'serve\.py' | grep -v grep | sed 's/^\([0-9]*\).*/\1/g' | xargs kill -9")
 
