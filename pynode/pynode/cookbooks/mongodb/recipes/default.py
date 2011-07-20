40f717042efe14e6a1a1bbd3320108e221b6490a
Travis Fischer <travis@stamped.com>
2011-07-20 19:26:34 -0400

    .

--- pynode/pynode/cookbooks/mongodb/recipes/default.py
+++ pynode/pynode/cookbooks/mongodb/recipes/default.py
@@ -3,8 +3,9 @@ from pynode import *
 #from pynode.resources import Package
 
 package = "mongodb"
-if env.system.platform != "mac_os_x":
-    package = "mongodb-10gen"
+# TODO: install 10gen's package
+#if env.system.platform != "mac_os_x":
+#    package = "mongodb-10gen"
 
 Package(package)
 
