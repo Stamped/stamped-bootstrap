1f4370a2047b530f9fbd57757005c9e827a5978d
Travis Fischer <travis@stamped.com>
2011-07-20 22:17:21 -0400

    .

--- cookbooks/stamped/recipes/default.py
+++ cookbooks/stamped/recipes/default.py
@@ -44,7 +44,7 @@ if 'db' in env.config.node.roles:
     
     env.cookbooks.mongodb.MongoDBConfigFile(**config)
     Service(name="mongod", 
-            start_cmd=r"ps -e | grep mongod | grep -v grep | sed 's/^\([0-9]*\).*/\1/g' | xargs kill -9; mongod --fork --replSet %s --config %s %s" % \
+            start_cmd=r"ps -e | grep mongod | grep -v grep | sed 's/^[ \t]*\([0-9]*\).*/\1/g' | xargs kill -9; mongod --fork --replSet %s --config %s %s" % \
             (config.replSet, config.path, string.joinfields(options, ' ')))
 
 if 'webServer' in env.config.node.roles:
