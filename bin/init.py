e2eee695e4c259e871baee3f01718d162e6039a0
Travis Fischer <travis@stamped.com>
2011-07-21 19:33:52 -0400

    .

--- bin/init.py
+++ bin/init.py
@@ -92,7 +92,7 @@ def replSetInit(config):
     utils.write(conf_path, conf_str)
     
     find_wsgi_server = r"ps -e | grep python | grep 'serve\.py' | grep -v grep"
-    if 0 == utils.shell3(find_wsgi_server):
+    if 0 == utils.shell2(find_wsgi_server, shell=True, stdout=PIPE, stderr=PIPE):
         utils.shell(r"%s | sed 's/^[ \t]*\([0-9]*\).*/\1/g' | xargs kill -9" % find_wsgi_server)
     
     utils.log("Running WSGI application server")
