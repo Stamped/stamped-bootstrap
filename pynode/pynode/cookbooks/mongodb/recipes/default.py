9b7c2e2cda5dd1a221915b84b019daedfb6eeb07
Travis Fischer <travis@stamped.com>
2011-07-20 20:50:52 -0400

    .

--- pynode/pynode/cookbooks/mongodb/recipes/default.py
+++ pynode/pynode/cookbooks/mongodb/recipes/default.py
@@ -8,9 +8,7 @@ if env.system.platform != "mac_os_x":
     Execute("sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
         not_if = "(apt-key list | grep 10gen.com > /dev/null)")
     
-    Execute("sudo cat 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' >> /etc/apt/sources.list", 
-        not_if = "(apt-key list | grep 10gen.com > /dev/null)")
-    
+    Execute("sudo cat 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' >> /etc/apt/sources.list")
     package = "mongodb-10gen"
 
 Package(package)
