483c5e16fd333fb67733ee9b34a8fb9f00500639
Travis Fischer <travis@stamped.com>
2011-07-26 00:09:03 -0400

    .

--- pynode/pynode/cookbooks/libevent/recipes/default.py
+++ pynode/pynode/cookbooks/libevent/recipes/default.py
@@ -9,7 +9,9 @@ from pynode.resources import *
 
 version = "libevent-2.0.12-stable"
 
-Package("gcc")
+if env.system.platform != "mac_os_x":
+    Package("gcc")
+
 Script(name="libevent", 
        code="""
 sudo wget http://monkey.org/~provos/%s.tar.gz && tar -xvf %s.tar.gz && cd %s && ./configure; make; make install; ldconfig
