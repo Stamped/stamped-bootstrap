dde3794db66919d3df81411f72d4f8ccaef48927
Kevin Palms <kevin@stamped.com>
2011-08-01 17:16:13 -0400

    Adding Ganglia scripts... cross your fingers

--- bin/ready.py
+++ bin/ready.py
@@ -6,15 +6,34 @@ __copyright__ = "Copyright (c) 2011 Stamped.com"
 __license__ = "TODO"
 
 import os
-from subprocess import Popen, PIPE
-from flask import request, Response, Flask
+# from subprocess import Popen, PIPE
+# from flask import request, Response, Flask
+# 
+# app = Flask(__name__)
+# 
+# @app.route('/')
+# def index():
+#     return "initialized"
+#  
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5001, threaded=True)
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "initialized"
- 
+def main():
+    if env.system.platform != 'mac_os_x':
+    
+        # Run as root!
+        bash = """
+            apt-get -y install ganglia-monitor
+            ps -e | grep gmond | grep -v grep | sed 's/^[ \t]*\([0-9]*\).*/\1/g' | xargs kill -9
+            mkdir /etc/ganglia/conf.d
+            mkdir /usr/lib64/ganglia/python_modules
+            cp /stamped/bootstrap/cookbooks/stamped/files/ganglia/gmond.conf /etc/ganglia/gmond.conf
+            cp /stamped/bootstrap/cookbooks/stamped/files/ganglia/modpython.conf /etc/ganglia/conf.d/modpython.conf
+            cp /stamped/bootstrap/cookbooks/stamped/files/ganglia/python_modules/* /usr/lib64/ganglia/python_modules
+            cp /stamped/bootstrap/cookbooks/stamped/files/ganglia/conf.d/* /etc/ganglia/conf.d
+            gmond
+        """
+    	os.system(bash)
+	
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, threaded=True)
-
+	main()
