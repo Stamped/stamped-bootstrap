1b600487ba33dbf9d59602939c4dddf7d4f52166
Kevin Palms <kevin@stamped.com>
2011-09-17 23:53:41 -0400

    Update to PCRE-8.13. Need to do this in a better way :)

--- cookbooks/stamped/recipes/default.py
+++ cookbooks/stamped/recipes/default.py
@@ -103,19 +103,19 @@ if 'webServer' in env.config.node.roles:
         cd %(path)s
         wget 'http://nginx.org/download/nginx-1.0.5.tar.gz'
         tar -xzvf nginx-1.0.5.tar.gz 
-        wget 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.10.tar.gz'
-        tar -xzvf pcre-8.10.tar.gz 
+        wget 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.13.tar.gz'
+        tar -xzvf pcre-8.13.tar.gz 
         wget 'http://zlib.net/zlib-1.2.5.tar.gz'
         tar -xzvf zlib-1.2.5.tar.gz 
         wget 'http://www.openssl.org/source/openssl-1.0.0d.tar.gz'
         tar -xzvf openssl-1.0.0d.tar.gz
         cd nginx-1.0.5/
-        ./configure --with-pcre=../pcre-8.10/ --with-zlib=../zlib-1.2.5/ --with-openssl=../openssl-1.0.0d --with-http_ssl_module
+        ./configure --with-pcre=../pcre-8.13/ --with-zlib=../zlib-1.2.5/ --with-openssl=../openssl-1.0.0d --with-http_ssl_module
         make
         mv objs/nginx %(path)s/bin/nginx
         cp conf/mime.types %(path)s/bin/
         cd ../
-        rm -rf nginx-1.0.5.tar.gz pcre-8.10.tar.gz zlib-1.2.5.tar.gz openssl-1.0.0d.tar.gz pcre-8.10/ zlib-1.2.5/ openssl-1.0.0d/
+        rm -rf nginx-1.0.5.tar.gz pcre-8.13.tar.gz zlib-1.2.5.tar.gz openssl-1.0.0d.tar.gz pcre-8.13/ zlib-1.2.5/ openssl-1.0.0d/
         """ % { 'path': env.config.node.path }
 
         Execute(r'. %s && %s' % (activate, cmd))
