10527a148f0491e05a4346dab98b0f418332278b
Travis Fischer <travis@stamped.com>
2011-10-31 18:20:02 -0400

    adding cron job to geocode custom entities

--- bin/cron.sh
+++ bin/cron.sh
@@ -2,3 +2,6 @@
 # everyday at midnight
 @daily . /stamped/bin/activate && python /stamped/bootstrap/bin/update_db.py  >> /stamped/logs/cron.log 2>&1
 
+# everyday at midnight
+@daily . /stamped/bin/activate && python /stamped/stamped/sites/stamped.com/bin/handle_custom_entities.py  >> /stamped/logs/handle_custom_entities.log 2>&1
+
