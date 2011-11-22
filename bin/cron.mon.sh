c7543119c39229e1eb1dd4929ce7ab0be4e8958e
Travis Fischer <travis@stamped.com>
2011-11-22 17:31:03 -0500

    storing more usage stats via 10 min cron job

--- bin/cron.mon.sh
+++ bin/cron.mon.sh
@@ -1,2 +1,4 @@
 */2 * * * * . /stamped/bin/activate && python /stamped/stamped/sites/stamped.com/bin/alerts/alerts.py >> /stamped/logs/alerts.log
 
+*/10 * * * * . /stamped/bin/activate && python /stamped/stamped/sites/stamped.com/bin/stats.py >> /stamped/logs/stats.log
+
