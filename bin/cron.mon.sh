f4d88376be2eba1578a7863999f21c91b54192de
Travis Fischer <travis@stamped.com>
2011-11-22 17:32:11 -0500

    .

--- bin/cron.mon.sh
+++ bin/cron.mon.sh
@@ -1,4 +1,4 @@
 */2 * * * * . /stamped/bin/activate && python /stamped/stamped/sites/stamped.com/bin/alerts/alerts.py >> /stamped/logs/alerts.log
 
-*/10 * * * * . /stamped/bin/activate && python /stamped/stamped/sites/stamped.com/bin/stats.py >> /stamped/logs/stats.log
+*/10 * * * * . /stamped/bin/activate && python /stamped/stamped/sites/stamped.com/bin/stats.py --store >> /stamped/logs/stats.log
 
